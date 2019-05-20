e4a8a466f896068e3568bd1cc7ae908e36c9312f
vicse <vicse.ore@tecsup.edu.pe>
2019-05-19 23:55:37 -0500

    Initial commit

---
+++ Laboratorio12/Laboratorio12/NumericValidationTriggerAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Laboratorio12
+{
+    class NumericValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry entry)
+        {
+            double result;
+            bool isValid = Double.TryParse(entry.Text, out result);
+            entry.TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+    
+}
